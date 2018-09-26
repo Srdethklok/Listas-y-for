9cf8ee3e7ea096129dfb96998402aba5f7c27c2e
Adrián <la.muerte.de.pandora@gmail.com>
2018-09-26 05:48:14 +0200

    Con el comando "for" conseguir lo mismo

--- Listas_y_for.py
+++ Listas_y_for.py
@@ -9,13 +9,7 @@ while input_usuario != "fin":
     if input_usuario != "fin":
         input_usuario = input("que mas añadir a la lita (escribe fin para salir): ")
 
-
-largo_lista = len(mi_lista)   #`len´sirve para saver el largo de una lista
-indice_actual = 0
-
-
-while indice_actual < largo_lista:
-    print("Tienes que {}".format(mi_lista[indice_actual]))
-    indice_actual += 1
+for item in mi_lista:
+    print("tienes que:{}".format(item))
 
 print("tu lista a finalizado")
